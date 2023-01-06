5e461019119567967edfc9e9047c1fcef34b89c9
wimsh <waheed.nazeer@gmail.com>
2023-01-06 23:02:01 +0500

    currentyear

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,4 @@
 const options={weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'}
+let currentyear={year: 'numeric'}
+document.getElementById('currentyear').textContent=new Date().toLocaleDateString('en-US', currentyear);
 document.getElementById('lastmodified').textContent=new Date(document.lastModified).toLocaleDateString('en-US', options);
