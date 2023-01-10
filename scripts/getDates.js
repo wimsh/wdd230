2625d84f226a1604ac10797c736c4f9e47935616
wimsh <waheed.nazeer@gmail.com>
2023-01-11 00:06:53 +0500

    index.html is updated with responsive JS code

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,3 +7,10 @@ document.getElementById("currentyear").innerHTML = currentYear;
 // Get Last Modified Date
 var lastModifiedDate = document.lastModified;
 document.getElementById("lastmodified").innerHTML = lastModifiedDate;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
--- week02/scripts/getDates.js
+++ week02/scripts/getDates.js
@@ -2,3 +2,10 @@ const options={weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', 
 let currentyear={year: 'numeric'}
 document.getElementById('currentyear').textContent=new Date().toLocaleDateString('en-US', currentyear);
 document.getElementById('lastmodified').textContent=new Date(document.lastModified).toLocaleDateString('en-US', options);
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
